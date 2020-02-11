96c4c70e28300f5cabd0bb2f9ced37237fa83276
Thomas Sullivan <tdsullivan812@gmail.com>
2020-02-11 16:24:05 -0500

    Creating script to read the names of sprites for character customization. Also created a JSON file and put it in StreamingAssets

---
+++ Hot Girl Summer Prototype 1/Assets/Scripts/Characteristic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Characteristic 
+{
+    public readonly Sprite[] listOfSprites;
+
+    public Sprite[] spritesOnDisplay = new Sprite[5];
+
+    protected int firstSpriteIndex;
+    protected int lastSpriteIndex;
+
+    public void IncrementDisplay(bool isLeft)
+    {
+        firstSpriteIndex = firstSpriteIndex + 1 % 5;
+        lastSpriteIndex = firstSpriteIndex;
+        for (int i = 0; i < 5; i++)
+        {
+            spritesOnDisplay[i] = lastSpriteIndex;
+            lastSpriteIndex = lastSpriteIndex + 1 % 5;
+        }
+    }
+}
---
+++ Hot Girl Summer Prototype 1/Assets/Scripts/CustomizableOptions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomizableOptions : MonoBehaviour
+{
+    public string streamingPath;
+    public static DataHolder file;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        file = new DataHolder();
+        streamingPath = Path.Combine(Application.streamingAssetsPath, "data.json");
+
+        StreamReader reader = new StreamReader(streamingPath);
+
+        string fileContents = reader.ReadToEnd();
+
+        file = DataHolder.CreateFromJSON(fileContents);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
