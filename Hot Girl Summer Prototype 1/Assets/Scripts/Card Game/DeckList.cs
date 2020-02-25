64846669cbe6f426466627aa7341a360a7caf0ab
Thomas Sullivan <tdsullivan812@gmail.com>
2020-02-25 15:26:13 -0500

    Organized Script folder and created files for card game classes

---
+++ Hot Girl Summer Prototype 1/Assets/Scripts/Card Game/DeckList.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckList
+{
+    public List<Card> allCards;
+    public Card.VictoryPoints victoryPoints;
+
+    public Card AddCard(Card cardToAdd)
+    {
+        return cardToAdd;
+    }
+
+    public Card RemoveCard(Card cardToRemove)
+    {
+        return cardToRemove;
+    }
+}
---
+++ Hot Girl Summer Prototype 1/Assets/Scripts/Card Game/Discard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Discard
+{
+    public List<Card> cardsInDiscard;
+
+    public Card RemoveFromDiscard(Card cardToRemove)
+    {
+        return cardToRemove;
+    }
+
+    public Card AddToDiscard(Card cardToAdd)
+    {
+        return cardToAdd;
+    }
+
+}
