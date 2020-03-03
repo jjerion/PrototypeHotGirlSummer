6bda404e5992e0012d5bd0da471d44cb531fe021
jjerion <56169080+jjerion@users.noreply.github.com>
2020-03-03 15:49:04 -0500

    added "calm" card scripts (effects still need updating) & added a script to load the card encounter, which is called at the end of your conversation with kelly

---
+++ Hot Girl Summer Prototype 1/Assets/Scripts/Card Game/Specific Cards/Chill/Chill.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chill : Card
+{
+    public Chill()
+    {
+        displayedInfo.cardName = "Chill";
+        displayedInfo.type = (int)Vibes.Calm;
+        displayedInfo.value = 1;
+        displayedInfo.isPlayable = true;
+        displayedInfo.text = "Gain 1 Calm Vibe Point next turn.";
+        displayedInfo.art = Resources.Load<Sprite>("");
+    }
+
+    public override void Effect()
+    {
+
+        //THOMAS HELP idk what to do here to add calm vibe point next turn
+        Debug.Log("played Chill");
+    }
+}
