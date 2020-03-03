5b23abf927e93e5341f4c35da96504d5e3db3890
MHOSS <46171808+HHOOSSPPEE@users.noreply.github.com>
2020-03-03 16:33:01 -0500

    Added hype cards and scoreboard

---
+++ Hot Girl Summer Prototype 1/Assets/Scripts/Card Game/Specific Cards/Hype/Encourage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encourage : Card
+{
+    public Encourage()
+    {
+        displayedInfo.cardName = "Encourage";
+        displayedInfo.type = (int)Vibes.Hype;
+        displayedInfo.value = 1;
+        displayedInfo.isPlayable = true;
+        displayedInfo.text = "The nest time the other character takes an action, they take it twice.";
+        displayedInfo.art = Resources.Load<Sprite>("");
+    }
+
+    public override void Effect()
+    {
+        Debug.Log("You are very cool.");        
+        //tells game to have NPC go twice next turn
+    }
+}
